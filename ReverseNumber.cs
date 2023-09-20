a27b802c1b127aa1699477036516e8b782791cec
Alishamhaske <mmhaske2001@gmail.com>
2023-09-20 16:54:53 +0530

    send

--- ReverseNumber.cs
+++ ReverseNumber.cs
@@ -9,37 +9,25 @@ namespace oopfirstclass_program.stringassignement
     public class ReverseNumber
     {
         //WAP to reverse the array itself, don’t print array in reverse – I want current array reverse. 
-
         public static void ReverseArray(int[] array)
         {
 
             int last = array.Length - 1;
             for (int i = 0; i < array.Length / 2; i++)
             {
-
-
                 int temp = array[i];
                 array[i] = array[last];
                 array[last] = temp;
                 last--;
-
-
             }
 
             for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(array[i]);
             }
-
-
         }
-
         public static void Main(string[] args)
         {
-
-
-            /* int[] array = new int[] { 3, 90, 45, 29, 37, 78 };
-             ReverseArray(array);*/
             int[] array = new int[] { 1, 2, 3, 4, 5 };
             ReverseArray(array);
         }
